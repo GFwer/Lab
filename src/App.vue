2d77c975afb7474a3ebcc7294ae0ffa9a8525c4b
龚方闻 <fangwen.gong@baishancloud.com>
2019-01-14 23:02:24 +0800

    font family

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="Fawen">
     <el-header>
-      <NavHeader/>
+      <NavHeader />
     </el-header>
-
     <!-- <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>|
       <router-link to="/fError">fError</router-link>
     </div>-->
     <el-container class="main-container">
-      <router-view/>
+      <router-view />
     </el-container>
     <el-footer class="footer">
       <div>All for fun!~</div>
-
       <a
         class="footer_text"
         href="http://www.miitbeian.gov.cn"
@@ -31,27 +29,41 @@ export default {
     NavHeader
   }
 };
-</script>
 
+</script>
 <style lang="less">
 html {
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
+
 .Fawen {
   height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
 }
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+
   &:before,
   &:after {
     box-sizing: border-box;
   }
+
+  font-family: "Helvetica Neue",
+  Helvetica,
+  "PingFang SC",
+  "Hiragino Sans GB",
+  "Microsoft YaHei",
+  "微软雅黑",
+  Arial,
+  sans-serif;
+
 }
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -61,19 +73,23 @@ html {
   display: flex;
   flex-direction: column;
 }
+
 .footer {
   display: flex;
   flex-direction: column;
   text-align: center;
   padding: 10px 0px;
   font-size: small;
-  > a {
+
+  >a {
     text-decoration: none;
     color: #409fff;
     opacity: 0.2;
   }
 }
+
 .main-container {
   flex: 1;
 }
+
 </style>
